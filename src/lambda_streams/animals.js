9d478ec3ba25efe785d85f16023d5dc56baa4719
Devon Brewster <devonbrewster42@gmail.com>
2020-07-02 13:24:01 -0500

    Did bonuses

--- src/lambda_streams/animals.js
+++ src/lambda_streams/animals.js
@@ -1,1 +1,62 @@
-//Bonus:  refactor Animals.java into javascript
+//Bonus:  refactor Animals.jaaa into jaaascript
+let animals = ["peacoCK", "rabbit", "chiwawa", "OranguTAN", "aipeR", "cobra", "paNDa", "bUffalo", "DeeR", "maLLard"];
+
+const capsFirst = (arr, mutate) => {
+    if (mutate) {
+        return arr.map(a => a.slice(0, 1).toUpperCase() + a.slice(1).toLowerCase())
+    }
+    else {
+        let newArr = arr.slice();
+        return newArr.map(a => a.slice(0, 1).toUpperCase() + a.slice(1).toLowerCase());
+    }
+}
+
+const lowerFirst = (arr, mutate) => {
+    if (mutate) {
+        return arr.map(a => a.slice(0, 1).toLowerCase() + a.slice(1).toUpperCase());
+    }
+    else {
+        let newArr = arr.slice();
+        return newArr.map(a => a.slice(0, 1).toLowerCase() + a.slice(1).toUpperCase());
+    }
+
+}
+
+const addAnimal = animalName => {
+    animals.push(animalName);
+    return animalName;
+}
+
+const flipAnimals = mutate => {
+    let instructions = ["Flip", "the", "animals", "list", ".", " ", "Mutate", "the", "animals", "only", "if", "flag", "true"];
+    if (mutate) {
+        instructions.reverse();
+        return instructions;
+    } else {
+        let newArray = instructions.slice();
+        newArray.reverse();
+        return newArray;
+    }
+}
+
+const sortAnimals = mutate => {
+    if (mutate) {
+        animals.sort();
+        return animals;
+    } else {
+        let newArr = animals.slice();
+        newArr.sort();
+        return newArr;
+    }
+}
+console.log("1", capsFirst(animals, false));
+console.log("2", capsFirst(animals, true));
+
+console.log("3", lowerFirst(animals, false));
+console.log("4", lowerFirst(animals, true));
+
+console.log(addAnimal("Doggy dog"));
+console.log("5", animals);
+animals = capsFirst(animals, true);
+console.log("6", flipAnimals());
+console.log("7", sortAnimals(false));
--- src/lambda_streams/numbers.js
+++ src/lambda_streams/numbers.js
@@ -1,1 +1,32 @@
-//Bonus: refactor Numbers.java into javascript
+//Bonus: refactor numbers.java into javascript
+let nums = [10, 100, 1000, 5, 50, 500, 3, 30, 300, 7, 70, 700, 1, 10, 100, 25, 250, 2500];
+
+const added = () => nums.reduce((a, b) => a + b, 0);
+console.log(added())
+
+const subtracted = () => nums.reduce((a, b) => a - b, 0);
+console.log(subtracted())
+
+
+const multiplied = () => nums.reduce((a, b) => a * b, 1);
+console.log(multiplied())
+
+
+const divided = () => nums.reduce((a, b) => a / b, multiplied());
+console.log(divided())
+
+const findMax = () => Math.max(...nums);
+console.log(findMax());
+const findMin = () => Math.min(...nums);
+console.log(findMin())
+
+const isPrime = num => {
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isPrime(29))
+console.log(isPrime(39))
